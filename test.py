a6fb5d82e76073a306631812dc84ecd4b278342d
Julián Tagliarini <6411322+jltaglia@users.noreply.github.com>
2022-09-05 12:23:00 -0300

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-dni = 23645093
-if len(dni) > 8 or not dni.isnumeric():
-    # EL DNI TIENE COMO MAX 8 NUMEROS
-    while True:
-        print(f'EL DNI no tiene el formato adecuado...')
-        dni = input('Ingrese un DNI como máximo 8 numeros: ')
-        break
